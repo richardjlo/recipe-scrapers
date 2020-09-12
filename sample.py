eafafe89de77b834cb2ab07f2fb9b0456ad1ba6d
richardjlo <richard@richardjlo.com>
2020-09-12 22:02:01 +0100

    updated formatting

--- sample.py
+++ sample.py
@@ -3,10 +3,13 @@ from recipe_scrapers import scrape_me
 # give the url as a string, it can be url from any site listed below
 scraper = scrape_me('https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/')
 
+print("\n- Title - ")
 print(scraper.title())
 # scraper.total_time()
 # scraper.yields()
-# scraper.ingredients()
+print("\n- Ingredients -")
+for x in scraper.ingredients():
+    print(x)
 # scraper.instructions()
 # scraper.image()
 # scraper.links()
