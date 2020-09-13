fe848321aed53ccf8cbc31b590b1b8ad4688072e
richardjlo <richard@richardjlo.com>
2020-09-13 17:02:03 +0100

    commented out image open

--- sample.py
+++ sample.py
@@ -2,7 +2,9 @@ import webbrowser
 from recipe_scrapers import scrape_me
 
 # give the url as a string, it can be url from any site listed below
-scraper = scrape_me('https://thewoksoflife.com/cumin-lamb-biang-biang-noodles/')
+# scraper = scrape_me('https://thewoksoflife.com/cumin-lamb-biang-biang-noodles/')
+scraper = scrape_me('https://thewoksoflife.com/cantonese-steamed-fish/')
+
 
 print("\n- Title - ")
 print(scraper.title())
@@ -11,6 +13,7 @@ print(scraper.title())
 print("\n- Ingredients -")
 for x in scraper.ingredients():
     print(x)
-# scraper.instructions()
-webbrowser.open(scraper.image())
+print("\n- Instructions -")
+print(scraper.instructions())
+# webbrowser.open(scraper.image())
 # scraper.links()
