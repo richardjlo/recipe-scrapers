a46217902e092161aa24ecd20bb866e2a7851812
richardjlo <richard@richardjlo.com>
2020-09-14 13:58:55 +0100

    removed starter code and updated homepage at /

--- api.py
+++ api.py
@@ -9,36 +9,7 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1><p>A prototype API for distant reading of science fiction novels.</p>'''
-
-
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    return jsonify(books)
-
-
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
-
-    # Create an empty list for our results
-    results = []
-
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
-
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
-    return jsonify(results)
+    return '''<h1>Cocina Recipe Scraper</h1><p>A prototype API for getting structured recipe data from food blogs.</p>'''
 
 @app.route('/api/v1/resources/recipes', methods=['GET'])
 def api_url():
@@ -57,7 +28,9 @@ def api_url():
     # attributes to the JSON format.
     return jsonify(
         title=scraper.title(),
+        photo=scraper.image(),
         yields=scraper.yields(),
+        ingredients=scraper.ingredients(),
         instructions=scraper.instructions(),
     )
     return jsonify(results)
